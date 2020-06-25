de7fdd55b48fe712b273eda07fc56258342092c8
Keith Tharp <keithstharp@gmail.com>
2020-06-25 12:38:26 -0400

    Length Selector active/works - axed provided JS

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-// Assignment Code
-/*
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-document.getElementById("generate").addEventListener("click", passwordFunction)
-*/
 
 // Setting strings for possible character types.
 let specialCharString = `!"#$%&'(\`)*+,-./:;<=>?@[\\]^_{|}~`;
@@ -30,42 +12,89 @@ let numericArray = numericString.split("");
 let lowerCaseArray = lowerCaseString.split("");
 let upperCaseArray = upperCaseString.split("");
 
+// base array to build password array onto
+let baseArray = [];
 
+// Activating the button
+let generateBtn = document.querySelector("#passwordGeneratorButton");
+generateBtn.addEventListener("click", displayPasswordResults);
 
+// This needs to accept user selections after .concat
+// Building the password array
+let passwordArray = baseArray.concat(numericArray, lowerCaseArray, specialCharArray, upperCaseArray);
 
-// function passwordFunction() {
-//   let passwordLength = prompt("Between 8 - 128, how long must your password be?");
-//   if (passwordLength < 8 || passwordLength > 128) {
-//     passwordFunction();
-//   }
-//   else {
-//     document.getElementById("password").innerHTML = passwordLength;
-//   }
+// Live feedback display for user selected password length.
+function genPass() {
+  passwordLength = document.getElementById("passLength").value;
+  document.getElementById("lengthDisplay").innerHTML = " " + passwordLength;
+}
+
+// Retrieving the password length.
+let passwordLength = document.getElementById("passLength").value;
+
+// initiate userPassword variable
+let userPassword = "";
+let randomMax = passwordArray.length;
+
+// This loop will build the password
+  for(i = 0; i < passwordLength; i++) {
+    let randomSelection = Math.floor(Math.random() * randomMax);
+    let passwordChar = passwordArray[randomSelection];
+
+    // add characters to userPassword
+    userPassword = userPassword.concat(passwordChar);
+  }
+  // Display the password
+  function displayPasswordResults() {
+  document.getElementById("passwordDisplay").innerHTML = userPassword;
+  }
+
+// working on retrieving and validating the checkboxes.
+
+//   let arrayBuild = null; 
+// for(let i = 0; i < 4; i++){
+//   let userParameters = document.getElementsById('characterType' + i);
+//       if(userParameters.checked){
+//         arrayBuild = userParameters[i].value;
+//            break;
+//       }
+// }
+
+// function testCheckbox(oCheckbox)
+// {
+//     var checkbox_val = oCheckbox.value;
+//     if (oCheckbox.checked == true)
+//     {
+//         alert("Checkbox with name = " + oCheckbox.name + " and value =" +
+//                 checkbox_val + " is checked");
+//     }
+//     else
+//     {
+//         alert("Checkbox with name = " + oCheckbox.name + " and value =" +
+//               checkbox_val + " is not checked");
+//     }
 // }
 
 
-
-/*
-Generate btn clicked:
-Question = Between 8 - 128, how long would you like your password to be?
-Questions = Capitals, numbers, special char
-*/
+// *** NOTES ***
 
 
-/*
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+/* Program process:
+initiate userPassword variable
+initiate character arrays
+initiate base array to build on
+1 - User clicks 'Generate Password' button.
+  Results in prompt with parameter questions.
+  Question 1 = How long would you like your password to be? >8 && <128
+  Question 2 = What types of characters would you like to include? = lowerCase, upperCase, numbers, special char
+2 - User types in number for password length, and selects character types (possibly Checkboxes?)
+  Validate password length input and assign to passwordLength variable
+  Build passwordArray based on user input
+----
+  Run function to generate password.
+  Loop iterates passwordLength
+  random passwordArray index chosen
+  character at random index concatenates to userPassword string
+----
+3 - Display generated password (userPassword)
 */
