9417f00c46fe8c6f6318d6f39f9c3993eb1eb3f9
Keith Tharp <keithstharp@gmail.com>
2020-07-01 09:20:24 -0400

    Copy icon added & working

--- script.js
+++ script.js
@@ -19,50 +19,54 @@ generateBtn.addEventListener('click', genCharArray);
 
 // Initiate the chosen password characters array to be filled by checkboxes.
 let passwordArray = [];
-    
-    // Assess the checkboxes and build the character array.
-    function genCharArray() {
-      //Clear the array
-      passwordArray = [];
-      if (document.getElementById("charType0").checked) {
-        passwordArray = passwordArray.concat(lowerCaseArray);
-      }
-      if (document.getElementById("charType1").checked) {
-        passwordArray = passwordArray.concat(upperCaseArray);
-      }
-      if (document.getElementById("charType2").checked) {
-        passwordArray = passwordArray.concat(numericArray);
-      }
-      if (document.getElementById("charType3").checked) {
-        passwordArray = passwordArray.concat(specialCharArray);
-      }
-      displayPasswordResults()
-  }
-  
-  // Live feedback display for user selected password length.
-  function genPassLength() {
-    passwordLength = document.getElementById("passLength").value;
-    document.getElementById("lengthDisplay").innerHTML = " " + passwordLength;
-  }
-  
-  // Retrieving the password length.
-  let passwordLength = document.getElementById("passLength").value;
 
-  // initializing display variable.
-  let passwordEl = document.getElementById("passwordDisplay");
+// Assess the checkboxes and build the character array.
+function genCharArray() {
+  //Clear the array
+  passwordArray = [];
+  if (document.getElementById("charType0").checked) {
+    passwordArray = passwordArray.concat(lowerCaseArray);
+  }
+  if (document.getElementById("charType1").checked) {
+    passwordArray = passwordArray.concat(upperCaseArray);
+  }
+  if (document.getElementById("charType2").checked) {
+    passwordArray = passwordArray.concat(numericArray);
+  }
+  if (document.getElementById("charType3").checked) {
+    passwordArray = passwordArray.concat(specialCharArray);
+  }
+  displayPasswordResults();
+}
+
+// Live feedback display for user selected password length.
+function genPassLength() {
+  passwordLength = document.getElementById("passLength").value;
+  document.getElementById("lengthDisplay").innerHTML = " " + passwordLength;
+}
+
+// Retrieving the password length.
+let passwordLength = document.getElementById("passLength").value;
+
+// initializing display variable.
+const passwordEl = document.getElementById("passwordDisplay");
+
+// initializing variable to help hide/display copy button
+const copyButton = document.getElementById("copyButton");
+
 
 function passwordGenerator() {
   let userPassword = [];
   // This loop will build the password
   for (i = 0; i < passwordLength; i++) {
-    
+
     let randomIndex = Math.floor(Math.random() * passwordArray.length);
     let passwordChar = passwordArray[randomIndex];
 
     // add characters to userPassword
     userPassword.push(passwordChar);
   }
-  return userPassword.join("")
+  return userPassword.join("");
 }
 
 // Display the password
@@ -71,13 +75,19 @@ function displayPasswordResults() {
 
   // dealing with display of "undefined" when no character set is chosen/
   if (passwordArray.length === 0) {
-    passwordEl.classList.add('alert'); 
+    passwordEl.classList.add('alert');
     passwordEl.innerHTML = "!- Select at least one character set -!";
     return;
   }
   //print the password to the screen.
   passwordEl.classList.remove('alert');
+  copyButton.classList.remove('hideButton');
   passwordEl.innerHTML = password;
+}
+
+function copyPassword() {
+  passwordEl.select();
+  document.execCommand("copy");
 }
 
 // *** NOTES ***
